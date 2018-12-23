0c90119dae0475eff00c0943a669b9ede1349f0b
강성찬 <tjdcks_12@naver.com>
2018-12-23 22:59:00 +0900

    [CREATE] init spring boot project

---
+++ src/main/java/com/sopt/dowadog/DowadogApplication.java
@@ -0,0 +1,14 @@
+package com.sopt.dowadog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DowadogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DowadogApplication.class, args);
+    }
+
+}
+
