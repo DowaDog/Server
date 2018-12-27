69133680fd06f70229217e1a1db8c90e7430a18d
강성찬 <tjdcks_12@naver.com>
2018-12-28 01:35:50 +0900

    [CREATE] JPA auditing 사용하여 createdAt, updatedAt 관리

---
+++ src/main/java/com/sopt/dowadog/model/domain/auditing/DateEntity.java
@@ -0,0 +1,30 @@
+package com.sopt.dowadog.model.domain.auditing;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@MappedSuperclass
+@EntityListeners(value = {AuditingEntityListener.class})
+@Data
+public class DateEntity{
+
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable=false, updatable=false)
+    @CreatedDate
+    private Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable=false)
+    @LastModifiedDate
+    private Date updatedAt;
+
+
+}
