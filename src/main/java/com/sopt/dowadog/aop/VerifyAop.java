c6733f35a9ce94ca92779893efa160c780301727
류수한 <ryusoohan@naver.com>
2018-12-28 20:44:49 +0900

    [CREATE] admin verify 기능 구현 시작

---
+++ src/main/java/com/sopt/dowadog/aop/VerifyAop.java
@@ -0,0 +1,16 @@
+//package com.sopt.dowadog.aop;
+//
+//import com.sopt.dowadog.model.common.DefaultRes;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.stereotype.Component;
+//
+//@Aspect
+//@Component
+//public class VerifyAop {
+//
+//    private final static String VERIFY = "verify";
+//
+//    private final static DefaultRes DEFAULT_RES = DefaultRes.builder().status(403).message("권한없음").build();
+//    private final static ResponseEntity
+//}
