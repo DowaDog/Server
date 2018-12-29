fdaa001b3618f2ea82d19117bfe709d3ba544b65
강성찬 <tjdcks_12@naver.com>
2018-12-29 22:56:25 +0900

    [UPDATE] paging 및 sorting 기능 이용하기 위한 상속 클래스 변경

--- src/main/java/com/sopt/dowadog/repository/CommunityRepository.java
+++ src/main/java/com/sopt/dowadog/repository/CommunityRepository.java
@@ -2,8 +2,8 @@ package com.sopt.dowadog.repository;
 
 import com.sopt.dowadog.model.domain.Community;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface CommunityRepository extends JpaRepository<Community, Integer>, JpaSpecificationExecutor<Community> {
+public interface CommunityRepository extends JpaRepository<Community, Integer>, PagingAndSortingRepository<Community, Integer> {
 
 }
