7dbe318b20906c128846d85829a62813db182705
류수한 <ryusoohan@naver.com>
2018-12-30 22:49:56 +0900

    [UPDATE] 오타수정, Atrribute 주석 추가

--- src/main/java/com/sopt/dowadog/controller/api/CommunityController.java
+++ src/main/java/com/sopt/dowadog/controller/api/CommunityController.java
@@ -10,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+
 @RequestMapping("api/community")
 @Controller
 public class CommunityController {
@@ -61,7 +63,9 @@ public class CommunityController {
 
     //특정 커뮤니티 글의 댓글 리스트 조회
     @GetMapping("/{communityId}/comments")
-    public ResponseEntity readCommentList(@PathVariable("communityId") int communityId) {
+    public ResponseEntity readCommentList(@PathVariable("communityId") int communityId, HttpServletRequest req) {
+
+        //req.getAttribute("name");
 
         return new ResponseEntity(communityCommentService.readCommunityCommentList(communityId), HttpStatus.CREATED);
     }
