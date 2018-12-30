d1c672561b29c91c1292e097e5f37378b55e3184
류수한 <ryusoohan@naver.com>
2018-12-30 22:46:53 +0900

    [UPDATE] ResponseMessage 추가

--- src/main/java/com/sopt/dowadog/util/ResponseMessage.java
+++ src/main/java/com/sopt/dowadog/util/ResponseMessage.java
@@ -23,7 +23,17 @@ public class ResponseMessage {
 
     //Cardnews
     public static final String READ_CARDNEWS = "카드뉴스 조회 성공";
+    public static final String CREATED_CARDNEWS = "카드뉴스 생성 성공";
+    public static final String DELETE_CARDNEWS = "카드뉴스 삭제 성공";
+    public static final String UPDATE_CARDNEWS = "카드뉴스 수정 성공";
+    public static final String NOT_FOUND_CARDNNEWS = "카드뉴스 정보를 찾을 수 없습니다.";
+
+    //카드뉴스 contents
     public static final String READ_CARDNEWSCONTENTS = "카드뉴스 컨텐츠 조회 성공";
+    public static final String CREATED_CARDNEWSCONTENTS = "카드뉴스 컨텐츠 생성 성공";
+    public static final String DELETE_CARDNEWSCONTENTS = "카드뉴스 컨텐츠 삭제 성공";
+    public static final String UPDATE_CARDNEWSCONTENTS = "카드뉴스 컨텐츠 수정 성공";
+    public static final String NOT_FOUND_CARDNEWSCONTENTS = "카드뉴스 컨텐츠 정보를 찾을 수 없습니다.";
 
     //Community
     public static final String READ_COMMUNITY = "커뮤니티 정보 조회 성공";
