8ba9f0f1cbcba8cb17d8ad06e291e71f3a7269bd
류수한 <ryusoohan@naver.com>
2018-12-31 18:40:43 +0900

    [UPDATE] userService에 필요없는 Create 삭제

--- src/main/java/com/sopt/dowadog/service/UserService.java
+++ src/main/java/com/sopt/dowadog/service/UserService.java
@@ -4,11 +4,15 @@ import com.sopt.dowadog.model.common.DefaultRes;
 import com.sopt.dowadog.model.domain.User;
 import com.sopt.dowadog.repository.UserRepository;
 import com.sopt.dowadog.util.ResponseMessage;
+import com.sopt.dowadog.util.S3Util;
 import com.sopt.dowadog.util.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.Id;
 import java.util.List;
 
 @Service
@@ -16,11 +20,6 @@ public class UserService {
 
     @Autowired
     UserRepository userRepository;
-
-    public DefaultRes<User> createUser(User user) {
-        return DefaultRes.res(StatusCode.OK, ResponseMessage.CREATED_USER, userRepository.save(user));
-
-    }
 
     public DefaultRes<List<User>> readUserList() {
         final List<User> userList = userRepository.findAll();
@@ -31,7 +30,6 @@ public class UserService {
     public List<User> readUserListOrdinary() {
         return userRepository.findAll();
     }
-
 
     public String a(){
         return null;
