82861d06599ccc1f24b4108936d441564960e4b1
강성찬 <tjdcks_12@naver.com>
2019-01-01 12:44:39 +0900

    [UPDATE] 복합키 테이블을 위한 equals, hashcode 오버라이딩

--- src/main/java/com/sopt/dowadog/model/domain/UserCardnewsId.java
+++ src/main/java/com/sopt/dowadog/model/domain/UserCardnewsId.java
@@ -13,4 +13,23 @@ public class UserCardnewsId implements Serializable {
     private String user;
     private Integer cardnews;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserCardnewsId that = (UserCardnewsId) o;
+        return Objects.equals(user, that.user) &&
+                Objects.equals(cardnews, that.cardnews);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(user, cardnews);
+    }
+
 }
