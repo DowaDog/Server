87bd21ed0a1cb90dd954f29814405ff364ea75b5
강성찬 <tjdcks_12@naver.com>
2019-01-01 13:24:39 +0900

    [UPDATE] 패키지 구조 변경

--- src/main/java/com/sopt/dowadog/controller/api/normal/AnimalController.java
+++ src/main/java/com/sopt/dowadog/controller/api/normal/AnimalController.java
@@ -1,4 +1,4 @@
-package com.sopt.dowadog.controller.api;
+package com.sopt.dowadog.controller.api.normal;
 
 
 import com.sopt.dowadog.model.dto.FilterDto;
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
-@RequestMapping("api/animals")
+@RequestMapping("api/normal/animals")
 @Controller
 public class AnimalController {
 
--- src/main/java/com/sopt/dowadog/repository/CommunityCommentRepository.java
+++ src/main/java/com/sopt/dowadog/repository/CommunityCommentRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommunityCommentRepository extends JpaRepository<CommunityComment, Integer> {
-    List<CommunityComment> findByCommunityIdOrderByCreatedAtDesc(int communityId);
+    List<CommunityComment> findByCommunityId(int communityId);
 }
