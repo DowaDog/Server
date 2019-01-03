a36df716847e43253e59e5deb931ca2ded1a585a
류수한 <ryusoohan@naver.com>
2019-01-03 13:19:05 +0900

    [DELETE] 본인인증 관련 코드 삭제

--- src/main/java/com/sopt/dowadog/exceptions/TokenVerificationException.java
+++
@@ -1,7 +0,0 @@
-package com.sopt.dowadog.exceptions;
-
-public class TokenVerificationException extends RuntimeException{
-    public TokenVerificationException(String message) {
-        super(message);
-    }
-}
