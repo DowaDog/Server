e4c26c7bbd50707fe3c379e669b8ad47814c08c2
류수한 <ryusoohan@naver.com>
2019-01-04 19:00:20 +0900

    [UPDATE] CARE 관련 수정

---
+++ src/main/java/com/sopt/dowadog/controller/api/care/CareController.java
@@ -0,0 +1,10 @@
+package com.sopt.dowadog.controller.api.care;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("api/care")
+public class CareController {
+}
