820588081ab20041731df737379784f2dff9f442
강성찬 <tjdcks_12@naver.com>
2019-01-10 15:18:22 +0900

    [UPDATE] 입양동물 정보 보여주는곳 animal type 추가

--- src/main/java/com/sopt/dowadog/controller/api/care/CareAnimalController.java
+++ src/main/java/com/sopt/dowadog/controller/api/care/CareAnimalController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("api/care/animals")
 @Controller
@@ -38,9 +35,9 @@ public class CareAnimalController {
         }
     }
 
-    @GetMapping
+    @GetMapping("{animalId}")
     public ResponseEntity animal(@RequestHeader(value = "Authorization", required = false) final String jwtToken,
-                                 @RequestParam("animalId") int animalId) {
+                                 @PathVariable(name = "animalId") int animalId) {
 
         try{
             Care care = careService.getCareByJwtToken(jwtToken);
