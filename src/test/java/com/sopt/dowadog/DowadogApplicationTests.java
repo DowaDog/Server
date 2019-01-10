7e3f0061c19ba282df02b65e3549bc4716f009d0
강성찬 <tjdcks_12@naver.com>
2019-01-11 05:31:45 +0900

    [CREATE] 푸시 기능 테스트코드 추가

--- src/test/java/com/sopt/dowadog/DowadogApplicationTests.java
+++ src/test/java/com/sopt/dowadog/DowadogApplicationTests.java
@@ -1,7 +1,9 @@
 package com.sopt.dowadog;
 
+import com.sopt.dowadog.util.AsyncUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -11,6 +13,16 @@ public class DowadogApplicationTests {
 
     @Test
     public void contextLoads() {
+        try{
+
+            AsyncUtil asyncUtil = new AsyncUtil();
+            System.out.println("#################### MESSAGE GO");
+            asyncUtil.sendOne("cIegScLjZlI:APA91bE3ZXq2iy2N-P47N5F7NwBjN6fwY0y0jw3oulTL_Mh3WQacuSF0vy9zJl6VJDBpEaXPPZ0ldHWNSEMA1g1-0sU6AuqvkQ3FTLxaHetqA7m-gcUAvz-kqLjpsAzlDL-O9aGt-L2w", "정신이", "나갔었나봐");
+            System.out.println("#################### MESSAGE END");
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
     }
 
 }
